e2afa24c01a7488ab390219bbedc6b230f83935a
Rafał Gosiewski <rafal.gosiewski@gmail.com>
2022-07-31 22:48:33 +0200

    5 exclude basic error controller

--- src/main/java/com/rgosiewski/frameiq/server/application/config/SwaggerConfig.java
+++ src/main/java/com/rgosiewski/frameiq/server/application/config/SwaggerConfig.java
@@ -2,15 +2,15 @@ package com.rgosiewski.frameiq.server.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableWebMvc
@@ -27,7 +27,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getApiInfo())
                 .select()
-                .paths(regex("/api/.*"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .build();
     }
 
